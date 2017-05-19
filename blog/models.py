03d091ef4750e086bc9e32103d9cd8ef0ec591fe
tfevan <tfevan@gmail.com>
2017-05-19 22:48:07 +0600

    Category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+class Category(models.Model):
+	user = models.ForeignKey('auth.user')
+	title = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.title
+
+class Post(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	title = models.CharField(max_length=250)
+	body = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+	tags = models.CharField(max_length=50)"""
