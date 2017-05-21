43adab6f49f11c568416a6bb40c750ecae6d1f64
tfevan <tfevan@gmail.com>
2017-05-21 23:47:00 +0600

    Template view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Category, Post
 
 
 def home(request):
-
-	return HttpResponse("hi") 
+	posts = Post.objects.all()
+	return render(request, 'blog/index.html', {'posts': posts}) 
 
 
 def detail(request, pk):
