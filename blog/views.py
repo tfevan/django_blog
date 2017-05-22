c4723caec2ab7421b6d869033ce1fee80905db79
tfevan <tfevan@gmail.com>
2017-05-22 21:43:30 +0600

    Paginator Post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Category, Post
 
 
 def home(request):
-	posts = Post.objects.all()
+	post_list = Post.objects.all()
+	pagination = Paginator(post_list, 2)
+
+	page = request.GET.get('page')
+	try:
+		posts = pagination.page(page)
+	except PageNotAnInteger:
+		posts = pagination.page(1)
+	except EmptyPage:
+		posts = pagination.page(pagination.num_pages)
+
 	return render(request, 'blog/index.html', {'posts': posts}) 
 
 
