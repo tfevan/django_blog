ef7c324b06deac93b693215690b7b26b60ea82fd
tfevan <tfevan@gmail.com>
2017-06-01 00:10:24 +0600

    get/_object_or_404 on detail

--- blog/views.py
+++ blog/views.py
@@ -28,8 +28,16 @@ def home(request):
 
 
 def detail(request, post_id):
-	post = Post.objects.get(pk=post_id)
+	post = get_object_or_404(Post, pk=post_id)
 	return render(request, 'blog/detail.html',{'post':post})
+
+
+
+
+
+
+
+
 
 def result(request, pk):
 	return HttpResponse('l')
